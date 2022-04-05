38751bdf5c35953a98e61f8ed3ab7bc060d104cc
1gorSantos <igor.souza@popcode.com.br>
2022-04-04 22:37:25 -0300

    going up DiceRoller project

---
+++ diceroller/src/main/java/com/igorsantos/shed/diceroller/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.igorsantos.shed.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var resultImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var botao: Button = findViewById(R.id.botao)
+        botao.setOnClickListener{
+            rollDice()
+        }
+        resultImage = findViewById(R.id.imagem)
+    }
+
+    private fun rollDice(){
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        resultImage.setImageResource(drawableResource)
+    }
+}
